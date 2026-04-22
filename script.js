b82120766d087bcbf1f29ca890bbb2ade99fb093
Sofiia <56766249+sofiazholud@users.noreply.github.com>
2026-04-22 17:13:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -24,4 +24,10 @@ const translations = {
 function setLanguage(lang) {
     document.getElementById("subtitle").textContent = translations[lang].subtitle;
     document.getElementById("studio-title").textContent = translations[lang].studioTitle;
-    document.getElementById("studio-text").textContent =
+    document.getElementById("studio-text").textContent = translations[lang].studioText;
+    document.getElementById("directions-title").textContent = translations[lang].directionsTitle;
+    document.getElementById("directions-list").innerHTML = translations[lang].directionsList.map(item => `<li>${item}</li>`).join("");
+    document.getElementById("projects-title").textContent = translations[lang].projectsTitle;
+    document.getElementById("contacts-title").textContent = translations[lang].contactsTitle;
+    document.getElementById("footer-text").textContent = translations[lang].footerText;
+}
