10017c2731d0fc9912706a13eb2f89c87504a5d2
Sofiia <56766249+sofiazholud@users.noreply.github.com>
2026-04-26 22:16:25 +0300

    Implement close modal on background click
    Added functionality to close modal when clicking outside of it.

--- script.js
+++ script.js
@@ -51,8 +51,19 @@ function setLanguage(lang) {
 function openModal(id) {
   document.getElementById(id).style.display = "block";
 }
+
 function closeModal(id) {
   document.getElementById(id).style.display = "none";
+}
+
+// Закриття при кліку на фон
+window.onclick = function(event) {
+  const modals = document.getElementsByClassName("modal");
+  for (let i = 0; i < modals.length; i++) {
+    if (event.target === modals[i]) {
+      modals[i].style.display = "none";
+    }
+  }
 }
 
 
